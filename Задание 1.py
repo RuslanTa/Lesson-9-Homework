3f209e750a3f5adff553a180085fd21083706cee
Ruslan Taganov <r.taganov.ruslan@gmail.com>
2022-01-26 17:16:21 +0300

    Задание 1

--- Задание 1.py
+++ Задание 1.py
@@ -4,18 +4,12 @@ b = int(input("Введите число B: "))
 
 def recursive(a, b):
     if a > b:
-        if a == b:
-            print(a)
-        else:
-            print(a)
-            recursive(a-1, b)
+        print(a)
+        recursive(a-1, b)
+    elif a < b:
+        recursive(a, b-1)
+        print(b)
     else:
-        if a < b:
-            if a == b:
-                print(a)
-            else:
-                recursive(a, b-1)
-                print(b)
-
+        print(a)
 
 recursive(a, b)
